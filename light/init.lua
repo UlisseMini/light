6f83dbc6f140cac632f023395fd2f962336de172
Ulisse Mini <ulisse.mini@gmail.com>
2021-09-04 13:44:55 -0400

    Add Value to init.lua

--- light/init.lua
+++ light/init.lua
@@ -3,5 +3,6 @@ local light = {}
 light.utils = require('light.utils')
 light.numeric = require('light.numeric')
 light.Tensor = require('light.tensor')
+light.Value = require('light.value')
 
 return light
