ef1a80777b848c7ace8444d2683e22200603cb9f
Ulisse Mini <ulisse.mini@gmail.com>
2021-09-04 14:30:49 -0400

    Patch table api for luajit

--- light/init.lua
+++ light/init.lua
@@ -1,5 +1,8 @@
 local light = {}
 
+if not table.unpack then table.unpack = unpack end
+if not table.pack then table.pack = function(...) return {...} end end
+
 light.utils = require('light.utils')
 light.numeric = require('light.numeric')
 light.Tensor = require('light.tensor')
