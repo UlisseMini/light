abe8b56d3a3b820f7c2076b09edfec459cf31b4d
Ulisse Mini <ulisse.mini@gmail.com>
2021-10-12 22:15:39 -0400

    Move to function, fix shapes

--- bench/purelua-matmul.lua
+++ bench/purelua-matmul.lua
@@ -1,11 +1,10 @@
-local images = {}
-
 local pixels = 28*28
 local classes = 10
 local batchsize = 50000
 
 print(('generating %s images of size %s (%s numbers)'):format(batchsize, pixels, batchsize*pixels))
 
+local images = {}
 for i=1,batchsize do
   images[i] = {}
   for j=1,pixels do
@@ -16,28 +15,35 @@ end
 print(('generating %s weights'):format(classes*pixels))
 local weights = {}
 
-for i=1,classes do
+for i=1,pixels do
   weights[i] = {}
-  for j=1,pixels do
+  for j=1,classes do
     weights[i][j] = math.random()
   end
+end
+
+local function matmul(A, B)
+  local n, m, p = #A, #A[1], #B[1]
+
+  local res = {}
+  for i=1,n do
+    res[i] = {}
+    for j=1,p do
+      local s = 0
+      local Ai = A[i]
+      for k=1,m do
+        s = s + Ai[k] * B[k][j]
+      end
+      res[i][j] = s
+    end
+  end
+  return res
 end
 
 print('evaluating model')
 local start = os.clock()
 
-local results = {}
-for i, image in ipairs(images) do
-  local scores = {}
-  for j=1,classes do
-    local s = 0
-    local weights_j = weights[j]
-    for k=1,pixels do
-      s = s + weights_j[k] * image[k]
-    end
-    scores[j] = s
-  end
-end
+local scores = matmul(images, weights)
 
 local took = os.clock() - start
 print(('took %.3fs'):format(took))
